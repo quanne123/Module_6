5efc2e87c78e90826c1659b90c365b6de0899ee6
TranVanAnhQuan <quandeptrai020901@gmail.com>
2023-05-19 06:23:49 +0700

    project

---
+++ nhop_nhep_hotpot_BE/src/main/java/com/example/nhop_nhep_hotpot_be/NhopNhepHotpotBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.nhop_nhep_hotpot_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhopNhepHotpotBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NhopNhepHotpotBeApplication.class, args);
+    }
+
+}
---
+++ nhop_nhep_hotpot_BE/src/test/java/com/example/nhop_nhep_hotpot_be/NhopNhepHotpotBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nhop_nhep_hotpot_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NhopNhepHotpotBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
